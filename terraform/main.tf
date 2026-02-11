adc4e5765d5100002b15d20367cdae809e0644d8
DevOps Engineer <devops@example.com>
2026-02-11 13:39:43 +0500

    Enable Docker build and EKS deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,7 +69,7 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name    = "${var.project_name}-cluster"
-  cluster_version = "1.28"
+  cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
